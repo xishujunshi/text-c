c1cd29036e0e1c81ee1eed51d0932cf6da77ac7e
xishujunshi <110795355+xishujunshi@users.noreply.github.com>
2023-01-10 20:07:48 +0800

    Add files via upload

---
+++ 1.10/1.10.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+//{
+//	int a, b, sum = 0;
+//	char c;
+//	scanf("%d", &a);
+//	sum = a;
+//	do {
+//		scanf("%c", &c);
+//		if (c == '=')
+//		{
+//			break;
+//		}
+//		scanf("%d", &b);
+//		if (c == '/' && b == 0)
+//		{
+//			printf("ERROR");
+//			goto out;
+//		}
+//		else if (c == '+')
+//		{
+//			sum += b;
+//		}
+//		else if (c == '-')
+//		{
+//			sum -= b;
+//		}
+//		else if (c == '*')
+//		{
+//			sum *= b;
+//		}
+//		else if (c == '/')
+//		{
+//			sum /= b;
+//		}
+//		else
+//		{
+//			printf("ERROR");
+//			goto out;
+//		}
+//	} while (1);
+//	printf("%d", sum);
+//out:
+//	return 0;
+//}*/int main()
+int main()
+{
+	int b, n;
+	scanf("%d",)
+	printf("%d %d",)
+	return 0;
+}
